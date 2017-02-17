d62206311894c37028fddb90c10ca432a860fb94
Ashley Kasim <ashleykasim1@gmail.com>
2017-02-16 21:42:13 -0500

    add fullcalendar component; remove protractor due to conflict with jquery

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,9 +1,29 @@
-import {Component} from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { CalendarComponent } from "angular2-fullcalendar/src/calendar/calendar";
 
 @Component({
     selector: 'menu',
     template: require('./menu.component.html')
 })
 
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  calendarOptions:Object
+
+  ngOnInit() {
+    if (this.calendarOptions === undefined) {
+      this.calendarOptions = {
+              height: 'auto',
+              fixedWeekCount : false,
+              defaultDate: new Date().toLocaleDateString(),
+              editable: true,
+              eventLimit: true, // allow "more" link when too many events
+              header: {
+                  left:   'title',
+                  center: '',
+                  right:  'today month basicWeek basicDay prev,next'
+              }
+            };
+    }
+  }
+
 }
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdCardModule, MdInputModule, MdGridListModule } from '@angular/material';
 import { MenuComponent } from './menu.component';
+import { CalendarComponent } from "angular2-fullcalendar/src/calendar/calendar";
 
 @NgModule({
-  imports: [CommonModule, MdCardModule, MdGridListModule, MdInputModule],
-  declarations: [MenuComponent],
+  imports: [
+    CommonModule,
+    MdCardModule,
+    MdGridListModule,
+    MdInputModule
+  ],
+  declarations: [MenuComponent, CalendarComponent],
   exports: [MenuComponent],
   providers: []
 })
