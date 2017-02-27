59a34d1c59d9b4bc81ad0a9312a1887d7e82c09c
Ashley Kasim <ashleykasim1@gmail.com>
2017-02-26 20:38:11 -0500

    fix truncate pipe

--- src/app/recipes/truncate.pipe.ts
+++ src/app/recipes/truncate.pipe.ts
@@ -2,21 +2,9 @@ import {Pipe} from '@angular/core';
 
 @Pipe({ name: 'truncate' })
 export class TruncatePipe {
-	transform(value: string, limit: number = 40, trail: String = '…'): string {
-		let result = value;
-
-		if (value) {
-			let words = value.split(/\s+/);
-			if (words.length > Math.abs(limit)) {
-				if (limit < 0) {
-					limit *= -1;
-					result = trail + words.slice(words.length - limit, words.length).join(' ');
-				} else {
-					result = words.slice(0, limit).join(' ') + trail;
-				}
-			}
-		}
-
-		return result;
-	}
+	transform(value: string, args: string) : string {
+	 let limit = parseInt(args)
+	 let trail = args.length > 1 ? args[1] : '...';
+	 return value.length > limit ? value.substring(0, limit) + trail : value;
+ }
 }
